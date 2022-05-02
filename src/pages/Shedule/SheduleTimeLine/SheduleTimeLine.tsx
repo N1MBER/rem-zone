57cfadedc2b813b505f9c4758dd88f975695de7a
N1MBER <socialfox33@gmail.com>
2022-05-02 16:24:27 +0300

    feat(ScheduleTimeLine): add calendar for date selection

--- src/pages/Shedule/SheduleTimeLine/SheduleTimeLine.tsx
+++ src/pages/Shedule/SheduleTimeLine/SheduleTimeLine.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { ViewMode } from '../../../types/timetable';
 import { Tabs } from '@consta/uikit/Tabs';
 import { getItems } from './helper';
 import { cn } from '../../../__private__/utils/bem';
 import { useRefSizes } from '../../../hooks/useRefSizes/useRefSizes';
 import { compareDates } from '../../../utils/date/date';
+import { Button } from '@consta/uikit/Button';
+import { IconCalendar } from '@consta/uikit/IconCalendar';
+import { useFlag } from '@consta/uikit/useFlag';
+import { Tooltip } from '@consta/uikit/Tooltip';
+import { DateTime } from '@consta/uikit/DateTimeCanary';
 
 import './SheduleTimeLine.scss';
 
@@ -36,6 +41,10 @@ export function SheduleTimeLine<TYPE extends ViewMode>(props: Props<TYPE>) {
   const [active, setActive] = useState<typeof items[number] | undefined>(
     items[0]
   );
+
+  const [showCalendar, setShowCalendar] = useFlag();
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const { width } = useRefSizes(containerRef);
 
@@ -80,12 +89,44 @@ export function SheduleTimeLine<TYPE extends ViewMode>(props: Props<TYPE>) {
         items={items}
         value={active}
         size="s"
+        className={cnSheduleTimeLine('Tabs')}
         fitMode="scroll"
         getLabel={(item) => item.label}
         onChange={({ value }) =>
           value && onChangeDate?.(value.date as ViewDate<TYPE>)
         }
       />
+      <Button
+        onlyIcon
+        view="secondary"
+        iconLeft={IconCalendar}
+        size="s"
+        ref={buttonRef}
+        onClick={setShowCalendar.toogle}
+      />
+      {showCalendar && (
+        <Tooltip
+          anchorRef={buttonRef}
+          direction="upStartRight"
+          possibleDirections={[
+            'downCenter',
+            'downLeft',
+            'leftDown',
+            'leftCenter',
+          ]}
+          onClickOutside={setShowCalendar.off}
+        >
+          <DateTime
+            type="month"
+            style={{
+              zIndex: 200,
+            }}
+            onChange={({ value }) =>
+              value && onChangeDate?.(value as ViewDate<TYPE>)
+            }
+          />
+        </Tooltip>
+      )}
     </div>
   );
 }
