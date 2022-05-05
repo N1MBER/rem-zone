5dbe6a753d8ea288072b0b7c562d64fa06532723
N1MBER <socialfox33@gmail.com>
2022-05-06 01:05:47 +0300

    feat(axios): add requests for brand

--- src/utils/api/routes/cars/cars.tsx
+++ src/utils/api/routes/cars/cars.tsx
@@ -2,13 +2,13 @@ import { AxiosPromise } from 'axios';
 import { instance } from '../..';
 import { endpoints } from '../../endpoints/endpoints';
 import { BaseListRequest, BaseListResponse } from '../../types';
-import { Auto } from '../../../../types/auto';
+import { Auto, Brand } from '../../../../types/auto';
 import { CreateAuto, UpdateAuto } from './types';
 
 // Car
 
 export const getCars = (
-  query: BaseListRequest & { newest?: boolean }
+  query: BaseListRequest
 ): AxiosPromise<BaseListResponse<Auto>> => {
   return instance.get(endpoints.cars.cars, {
     params: query,
@@ -32,4 +32,33 @@ export const getCar = (id: string): AxiosPromise<Auto> => {
 
 export const deleteCar = (id: string): AxiosPromise => {
   return instance.delete(`${endpoints.cars.cars}${id}/`);
+};
+
+// Brands
+
+export const getBrands = (
+  query: BaseListRequest
+): AxiosPromise<BaseListResponse<Brand>> => {
+  return instance.get(endpoints.cars.brand, {
+    params: query,
+  });
+};
+
+export const addBrand = (data: Omit<Brand, 'id'>): AxiosPromise<CreateAuto> => {
+  return instance.post(endpoints.cars.brand, { ...data });
+};
+
+export const updateBrand = (
+  data: Omit<Brand, 'id'>,
+  id: string
+): AxiosPromise<Brand> => {
+  return instance.put(`${endpoints.cars.brand}${id}/`, { ...data });
+};
+
+export const getBrand = (id: string): AxiosPromise<Brand> => {
+  return instance.get(`${endpoints.cars.brand}${id}/`);
+};
+
+export const deleteBrand = (id: string): AxiosPromise => {
+  return instance.delete(`${endpoints.cars.brand}${id}/`);
 };
